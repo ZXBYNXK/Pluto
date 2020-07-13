9ae36db9b7107a485b7283d6abdaa008ea56a0e5
DariusRain <dariusrain.student@careerdevs.com>
2020-07-13 06:19:59 -0400

    Created auth middleware and can determine if user is a user and return any private data.

---
+++ middleware/auth.js
@@ -0,0 +1,20 @@
+const {verify} = require("jsonwebtoken");
+const {jwtSec} = require("../config");
+module.exports = (req, res, next) => {
+    // Get token from header
+    const token = req.header("x-auth-token");
+    // Check if not token
+    if(!token)
+    {
+        res.status(401).json({msg: "No token, authorization denied."})
+    }
+
+    // Verify token
+    try {
+        const decoded = verify(token, jwtSec);
+        req.user = decoded;
+        next();
+    } catch (err) {
+        res.status(401).json({msg: "Token is not valid."})
+    }
+}
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,9 +2,32 @@
 const { Router } = require("express");
 const router = Router();
 
+// Models
+const User = require("../../models/User");
+
+// Middleware
+const auth = require("../../middleware/auth");
+
 // @route GET api/auth
-// @desc  Test Route
-// @access Public
-router.get("/", (req, res) => res.status(200).json({ test: true }));
+// @desc  Get user data by token
+// @access Private
+router.get("/", auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.status(200).json(user);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).json({msg: "Server Error"});
+    }
+
+});
+
+
 
 module.exports = router;
+
+
+// @route GET api/auth
+// @desc  Test Token
+// @access Private
+// router.get("/", auth, (req, res) => res.status(200).json({ test: true }));
