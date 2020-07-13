9f03bd0a53f6c643a58173e33fbf955088e53250
DariusRain <dariusrain.student@careerdevs.com>
2020-07-13 04:33:11 -0400

    Connected application to database.

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const server = express();
+const connectDB = require("./config/db")
 const PORT = process.env.PORT || 5000;
 server.get("/", (req, res) => res.status(200).json({ test: true }));
+connectDB();
 server.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
 });
