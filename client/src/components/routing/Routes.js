883488ca2d79f71af3bbb4eaffa7f9de1a3ad433
DariusRain <dariusrain.student@careerdevs.com>
2020-07-20 15:07:59 -0400

    Changed routing component and added NotFound component

--- client/src/components/routing/Routes.js
+++ client/src/components/routing/Routes.js
@@ -4,14 +4,15 @@ import Register from '../auth/Register';
 import Login from '../auth/Login';
 import Alert from '../layout/Alert';
 import Dashboard from '../dashboard/Dashboard';
+import ProfileForm from '../profile-forms/ProfileForm';
 import AddExperience from '../profile-forms/AddExperience';
 import AddEducation from '../profile-forms/AddEducation';
+import Profiles from '../profiles/Profiles';
+import Profile from '../profile/Profile';
+import Posts from '../posts/Posts';
+import Post from '../post/Post';
+import NotFound from '../layout/NotFound';
 import PrivateRoute from '../routing/PrivateRoute';
-// import Profiles from '../profiles/Profiles';
-// import Profile from '../profile/Profile';
-// import Posts from '../posts/Posts';
-// import Post from '../post/Post';
-// import NotFound from '../layout/NotFound';
 
 const Routes = props => {
   return (
@@ -20,14 +21,16 @@ const Routes = props => {
       <Switch>
         <Route exact path="/register" component={Register} />
         <Route exact path="/login" component={Login} />
+        <Route exact path="/profiles" component={Profiles} />
+        <Route exact path="/profile/:id" component={Profile} />
         <PrivateRoute exact path="/dashboard" component={Dashboard} />
+        <PrivateRoute exact path="/create-profile" component={ProfileForm} />
+        <PrivateRoute exact path="/edit-profile" component={ProfileForm} />
         <PrivateRoute exact path="/add-experience" component={AddExperience} />
         <PrivateRoute exact path="/add-education" component={AddEducation} />
-        {/* <PrivateRoute exact path="/posts/:id" component={Post} /> */}
-        {/* <Route component={NotFound} /> */}
-        {/* <Route exact path="/profiles" component={Profiles} /> */}
-        {/* <PrivateRoute exact path="/posts" component={Posts} /> */}
-        {/* <Route exact path="/profile/:id" component={Profile} /> */}
+        <PrivateRoute exact path="/posts" component={Posts} />
+        <PrivateRoute exact path="/posts/:id" component={Post} />
+        <Route component={NotFound} />
       </Switch>
     </section>
   );
