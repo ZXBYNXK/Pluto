f3a5720f9106e18874943efd5c6ed7b95ce98dd0
DariusRain <dariusrain.student@careerdevs.com>
2020-07-20 12:53:47 -0400

    Updating DashBoardActions Component.

--- client/src/components/dashboard/DashBoardActions.js
+++ client/src/components/dashboard/DashBoardActions.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export const DashBoardActions = () => {
+const DashBoardActions = () => {
   return (
     <div className="dash-buttons">
       <Link to="/edit-profile" className="btn btn-light">
