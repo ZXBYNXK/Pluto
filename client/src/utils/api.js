50fb3f1485c8620f02ed106b5711737dc3fb94fc
DariusRain <dariusrain.student@careerdevs.com>
2020-08-15 03:13:15 -0400

    V1.0.0 official deployment version.

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,21 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `${process.env.API_ROOT}/api`,
+  baseURL: `https://pluto-back-end.ue.r.appspot.com/api`,
   headers: {
     'Content-Type': 'application/json',
     'access-control-allow-origin': "*"
   }
 });
-
-// api.interceptors.response.use(
-//   res => res,
-//   err => {
-//     if (err.response.data.msg === 'Token is not valid') {
-//       store.dispatch({ type: LOGOUT });
-//     }
-//     return Promise.reject(err);
-//   }
-// );
 
 export default api;
